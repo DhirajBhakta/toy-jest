d631ca071bebdb6d1de66ca3358e706a965fa565
Dhiraj <dhirajbhakta110@gmail.com>
2021-08-21 22:42:54 +0530

    Files read using Haste jest-haste-map
    using jest-worker to parallelize reading test files
    just eval'ing the code inside tests for now

---
+++ index.mjs
@@ -0,0 +1,57 @@
+import glob from 'glob';
+import Haste from 'jest-haste-map';
+
+// to get #cpus to know the number of workers
+import { cpus } from 'os';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
+
+// this is much slower, hence we dont use it and we use haste-map
+const allFiles = glob.sync("**/*.test.js");
+
+// dirname of our index.mjs
+const root = dirname(fileURLToPath(import.meta.url))
+
+// this is much faster than glob
+// haste does much better caching of _which files have changed_ and stuff
+const hasteMap = new Haste.default({
+    rootDir: root,
+    roots: [root],
+    maxWorkers: cpus().length,
+    platforms: [],
+    extensions: ['js'],
+    name: 'best-cache',
+    // watch:true
+})
+
+// hasteFS is in-memory file system
+// but what if the file changes on the real file system?
+// haste has a watch mode to update the internal representation in-memory whenever the real file changes
+const { hasteFS } = await hasteMap.build();
+const testFiles = hasteFS.matchFilesWithGlob(['**/*.test.js']);
+
+// to read the test files
+import {runTest} from './worker.js'
+
+// read the test files | singe threaded
+for await(const testFile of testFiles){
+    console.log(await runTest(testFile))
+}
+
+import {Worker} from 'jest-worker';
+
+const worker = new Worker(join(root, 'worker.js'),{
+    enableWorkerThreads: true
+})
+// read the test files | multi threaded
+for await(const testFile of testFiles){
+    const {success, errorMessage} = await worker.runTest(testFile);
+
+    if(!success)
+        console.log(errorMessage)
+}
+
+worker.end();
+
+
---
+++ worker.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+
+exports.runTest = async (testFile) =>{
+    const code = await fs.promises.readFile(testFile, 'utf-8')
+    const testResult = {
+        success: false,
+        errorMessage: null
+    }
+    try{
+        eval(code);
+        testResult.success = true;
+    }catch(error){
+        testResult.errorMessage = error.message
+    }
+    return testResult
+}
