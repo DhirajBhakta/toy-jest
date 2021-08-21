9bdf1e4a77a36549f45031718df9d7496926e96e
Dhiraj <dhirajbhakta110@gmail.com>
2021-08-21 23:24:50 +0530

    moment of truth: assertion libraries| how describe and it and test is available globally inside test files

--- worker.js
+++ worker.js
@@ -1,15 +1,21 @@
 const fs = require('fs');
-
-exports.runTest = async (testFile) =>{
+exports.runTest = async (testFile) => {
     const code = await fs.promises.readFile(testFile, 'utf-8')
     const testResult = {
         success: false,
         errorMessage: null
     }
-    try{
+    try {
+        // expect(1).toBe(1)
+        const expect = (recievedValue) => ({
+            toBe: (expectedValue) => {
+                if (expectedValue !== recievedValue)
+                    throw new Error(`Expected ${expectedValue} but received ${recievedValue}`)
+            }
+        })
         eval(code);
         testResult.success = true;
-    }catch(error){
+    } catch (error) {
         testResult.errorMessage = error.message
     }
     return testResult
