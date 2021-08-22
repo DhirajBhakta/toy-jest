18230323a9e4e20e0221f46876a3d8ffe38249de
Dhiraj <dhirajbhakta110@gmail.com>
2021-08-22 13:30:41 +0530

    use jest-node-environment instead of passing context directly into vm; custom require implementation for test files

--- tests/describe.test.js
+++ tests/describe.test.js
@@ -1,4 +1,10 @@
 // tests/circus.test.js
+
+const banana = require("./banana.js");
+it("tastes good", ()=>{
+    expect(banana).toBe("good")
+
+})
 describe('describe test', () => {
     it('works', () => {
         expect(1).toBe(1);
@@ -8,6 +14,6 @@ describe('describe test', () => {
 describe('second describe test', () => {
     it(`doesn't work`, async () => {
         await new Promise(resolve=> setTimeout(resolve, 3000))
-        expect(1).toBe(2);
+        expect(1).toBe(1);
     });
 })
--- worker.js
+++ worker.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const expect = require('expect');
-const {describe, it, run, resetState} = require('jest-circus');
+const {dirname, join} = require("path");
+const {describe, it, run, resetState } = require('jest-circus');
 const vm = require('vm');
+const NodeEnvironment = require("jest-environment-node")
 const secret = "my_secret"
 exports.runTest = async (testFile) => {
     const code = await fs.promises.readFile(testFile, 'utf-8')
@@ -11,10 +13,15 @@ exports.runTest = async (testFile) => {
     }
     let testName = '';
     try {
-        const context = { describe, it , expect, setTimeout};
-        vm.createContext(context)
+        const customRequire = (fileName) =>{
+            const code = fs.readFileSync(join(dirname(testFile), fileName), 'utf-8');
+            vm.runInContext(`const module={exports:{}};  \n ${code};\n  module.exports`, environment.getVmContext())
+        }
+        const environment = new NodeEnvironment({
+            testEnvironmentOptions:{describe, it , expect, require:customRequire, console}
+        })
         resetState();
-        vm.runInContext(code, context)
+        vm.runInContext(code, environment.getVmContext())
         const {testResults} = await run();
         testResult.testResults = testResults;
         testResult.success = testResults.every((results) => !results.errors.length)
